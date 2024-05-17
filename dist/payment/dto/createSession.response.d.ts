7031577d8bc9cd9865f3309afbe2eda8ec0a0b36
mohammadnuridin <developer.mohammadnuridin@gmail.com>
2024-05-17 12:17:42 +0700

    #

---
+++ dist/payment/dto/createSession.response.d.ts
@@ -0,0 +1,3 @@
+export declare class CreateSessionResponseDto {
+    url: string;
+}
---
+++ dist/product/entities/product.entity.d.ts
@@ -0,0 +1,10 @@
+export declare class Product {
+    id: number;
+    description: string;
+    price: number;
+    quantity?: number;
+    name: string;
+    image: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
