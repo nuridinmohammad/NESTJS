9dd843549f2a272740d2b7ffaf1f44dc08acafc4
mohammadnuridin <developer.mohammadnuridin@gmail.com>
2024-05-17 12:25:45 +0700

    init

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,6 +10,7 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const chat_gateway_1 = require("./chat/chat.gateway");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -17,7 +18,7 @@ exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [],
         controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        providers: [app_service_1.AppService, chat_gateway_1.ChatGateway],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
